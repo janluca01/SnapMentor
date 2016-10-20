b0a5de5d2b2320f8b3860cedd84ef2d09b67c3f5
Jan-Luca Pfeiffer <janluca@gmail.com>
2016-10-20 23:55:59 +0200

    Redirect Issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 	
 	def after_sign_up_path_for(resource)
- 		"https://www.snapmentor.de/#aftersu"
+ 		"https://www.snapmentor.de/#aftersignup"
 	end
 
 end
